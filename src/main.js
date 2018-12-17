a110262bf470c743ef86c95d170a00ba7eb97292
V1kingGit <45792897+V1kingGit@users.noreply.github.com>
2018-12-17 11:44:46 +0100

    Added Tasks

--- src/main.js
+++ src/main.js
@@ -4,19 +4,102 @@ var
   benchmark = 0
 ;
 
+const	TASK_HARVEST	1
+const	TASK_TRANSFER	2
+
+const	PRIORITY_LOW	1
+const	PRIORITY_MEDIUM	2
+const	PRIORITY_HIGH	3
+
+// Sysselmann.newTask(TASK_HARVEST, 'creep', sources[0].id, sources[0].pos, PRIORITY_MEDIUM);
+
+function canDoTask(role, task)
+{
+	switch(task)
+	{
+		case TASK_HARVEST:
+		{
+			switch(role)
+			{
+				case 'harvester': return true;
+				default: return false;
+			}
+		}
+		default: return false;
+	}
+}
+
+RoomObject.prototype.createTask = function(vTask, vName, vTargetid, vTargetPos, vPriority)
+{
+	let	freeSlot,
+		taskData = { task: vTask, name: vName, targetid:, vTargetid, targetPos: vTargetPos, priority: vPriority };
+	for(i = 0; i < this.memory.tasks; i ++)
+	{
+		if(!this.memory.tasks[i])
+		{
+			freeSlot = i;
+			break;
+		}
+	}
+	this.memory.tasks[freeSlot] = taskData;
+}
+
+function newTask(creep)
+{
+	let sysselmann = creep.memory.sysselmann,
+		assigned[10],
+		priorityLevel = PRIORITY_LOW;
+	for(i = 0; i < sysselmann.memory.tasks.length; i ++)
+	{
+		if(sysselmann.memory.tasks[i].priority < priorityLevel) continue;
+		if(canDoTask(creep.memory.role, sysselmann.memory.tasks[i]))
+		{
+			for(x = 0; x < assigned.length; x ++)
+			{
+				if(!assigned)
+				{
+					assigned[x] = sysselmann.memory.tasks[i];
+					if(sysselmann.memory.tasks[i].priority > priorityLevel)
+					{
+						assigned = [];
+						priorityLevel = sysselmann.memory.tasks[i].priority;
+					}
+					break;
+				}
+			}
+		}
+	}
+	creep.memory.task = findClosestByRange(assigned.targetPos);
+}
+
 Creep.prototype.task = function(assignment)
 {
 	this.memory.task = assignment;
 }
 
-task.prototype.harvest = function()
+Creep.prototype.run = function()
 {
 	
 }
 
 module.exports.loop = function()
 {
-	
+	for(var name in Memory.creeps)
+	{
+		if(!Game.creeps[name])
+		{
+			delete Memory.creeps[name];
+		}
+		else
+		{
+			let creep = Game.creeps[name];
+			if(creep.isIdle)
+			{
+				newTask(creep);
+			}
+			creep.run();
+		}
+	}
 	
 	// -- Info Log --
 	benchmark = benchmark + Game.cpu.getUsed();
@@ -24,7 +107,7 @@ module.exports.loop = function()
 	{
 		moduleBaseBuilding.run(spawn1.room.controller.level, spawn1)
 		console.log('_____[ ' + Date() + ' ]_____')
-		for(var i in Game.rooms)
+		for(let i in Game.rooms)
 		{
 			roomAmount ++;
 			console.log('| Room ' + i + ' | Creeps:[' + Game.rooms[i].find(FIND_MY_CREEPS).length + '] Workers:[' + NaN + ']');
