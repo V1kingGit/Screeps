23d22194a629afa7f25540a5a499af70d97c2fad
V1kingGit <45792897+V1kingGit@users.noreply.github.com>
2018-12-20 14:27:59 +0100

    Create Jarl.js

---
+++ src/Jarldom/Jarl.js
@@ -0,0 +1,14 @@
+let adminJarl = 
+{
+	run: function()
+	{
+		for(let roomName in Game.rooms)
+		{
+			if(Game.rooms[roomName] == HAS SPAWN)
+			Memory.jarldom[roomName] = {}
+
+		}
+	}
+}
+
+module.exports = adminJarl
